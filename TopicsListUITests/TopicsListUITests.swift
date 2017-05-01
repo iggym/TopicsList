133ddb92d318943252a4cf18aa31d107397b532e
iggym <iggy.mw@gmail.com>
2017-04-30 22:02:36 -0500

    added some recorded tests

--- TopicsListUITests/TopicsListUITests.swift
+++ TopicsListUITests/TopicsListUITests.swift
@@ -31,6 +31,60 @@ class TopicsListUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let addButton = app.navigationBars["The List"].buttons["Add"]
+        addButton.tap()
+        
+        let aKey = app.keys["a"]
+        aKey.tap()
+        
+        let textField = app.alerts["New Topic"].collectionViews.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .textField).element
+        textField.typeText("a")
+        app.keys["space"].tap()
+        textField.typeText(" ")
+        
+        let tKey = app.keys["t"]
+        tKey.tap()
+        textField.typeText("t")
+        
+        let oKey = app.keys["o"]
+        oKey.tap()
+        textField.typeText("o")
+        app.typeText("p")
+        
+        let table = app.otherElements.containing(.navigationBar, identifier:"The List").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .table).element
+        table.tap()
+        app.typeText("i")
+        table.tap()
+        app.typeText("c")
+        table.tap()
+        addButton.tap()
+        aKey.tap()
+        textField.typeText("a")
+        app.keys["n"].tap()
+        textField.typeText("n")
+        oKey.tap()
+        textField.typeText("o")
+        tKey.tap()
+        textField.typeText("t")
+        app.keys["h"].tap()
+        textField.typeText("h")
+        app.keys["e"].tap()
+        textField.typeText("e")
+        app.keys["r"].tap()
+        textField.typeText("r")
+        app.typeText(" ")
+        table.tap()
+        app.typeText("t")
+        table.tap()
+        app.typeText("o")
+        table.tap()
+        app.typeText("pi")
+        table.tap()
+        app.typeText("c")
+        table.tap()
+        
     }
     
 }
